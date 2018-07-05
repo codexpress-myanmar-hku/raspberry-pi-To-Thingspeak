65b24a754c4a3751afce32e3f85ec2de4a61ea05
utkarsh867 <utkarsh867@gmail.com>
2018-07-05 22:00:08 +0530

    indentation at WaterTemperatureSensor.java

--- in/weargenius/hardware/WaterTemperatureSensor.java
+++ in/weargenius/hardware/WaterTemperatureSensor.java
@@ -7,18 +7,18 @@ import com.pi4j.temperature.TemperatureScale;
 
 public class WaterTemperatureSensor {
 	public double getWaterTemperature(){
-		
-        W1Master w1Master = new W1Master();
 
-        System.out.println(w1Master);
+		W1Master w1Master = new W1Master();
 
-        for (TemperatureSensor device : w1Master.getDevices(TemperatureSensor.class)) {
-            System.out.printf("%-20s %3.1f%3.1f\n", device.getName(), device.getTemperature(),
-                    device.getTemperature(TemperatureScale.CELSIUS));
-            if(device.getName().contains("28-0115155154ff"))
-            	return device.getTemperature(TemperatureScale.CELSIUS);
-        	//return 32;
-        }
+		System.out.println(w1Master);
+
+		for (TemperatureSensor device : w1Master.getDevices(TemperatureSensor.class)) {
+			System.out.printf("%-20s %3.1f%3.1f\n", device.getName(), device.getTemperature(),
+					device.getTemperature(TemperatureScale.CELSIUS));
+			if(device.getName().contains("28-0115155154ff"))
+				return device.getTemperature(TemperatureScale.CELSIUS);
+			//return 32;
+		}
 		return 0;
 	}
 
